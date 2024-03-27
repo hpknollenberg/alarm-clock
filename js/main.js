23758c1559cdd8c326cd92a6962fa16dfd4ef51e
hpknollenberg <hpknollenberg@gmail.com>
2024-03-27 13:46:44 -0400

    Base alarm clock finished.

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let alarmTime;
+const setAlarmButton = document.querySelector("#btn");
+
+function updateTime() {
+    //Get time
+    const date = new Date();
+    let [hours, minutes, seconds] = [
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds()
+    ];
+
+    //Formatting for hours, minutes, and seconds value
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    //Display time
+    document.getElementById("clock").innerHTML =
+        hours + ":" +
+        minutes + ":" +
+        seconds;
+
+
+    //Alarm
+ 
+    let convertedTime = (hours * 3600) + (minutes * 60) + seconds;
+
+    setAlarmButton.addEventListener("click", () => {
+        alarmTime = (hours * 3600) + (minutes * 60) + seconds + 10;
+    });
+
+    if (alarmTime == convertedTime) {
+        alert("It is " + hours + ":" + minutes + ":" + seconds + ".");
+        alarmTime = undefined;
+    }
+}
+
+setInterval(updateTime, 1000);
+
+
